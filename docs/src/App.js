ba4153e8009ec8882bf5351dce3ff1fc35fc20dc
alexsyo <alex_ok@hotmail.it>
2018-05-29 22:23:35 +0200

    removed test
    feature: master

--- docs/src/App.js
+++ docs/src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
         alignItems: 'center',
         justifyContent: 'center'
       }}>
-      test
         <CircularSlider
           onChange={value => this.setState({value})}
         />
